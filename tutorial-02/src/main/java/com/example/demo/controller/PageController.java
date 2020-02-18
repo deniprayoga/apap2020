8c83d2a51554a23d7cb719675441e30e5b68e49a
Deni Prayoga <deni.prayoga@ui.ac.id>
2020-02-18 18:21:30 +0700

    add tutorial-02

---
+++ tutorial-02/src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "hello";
+	}
+	
+	@RequestMapping("/hello2")
+	public String hello2 (@RequestParam(value="name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello2";
+	}
+	
+	@RequestMapping("/hello2/{name}")
+	public String helloPath(@PathVariable Optional<String> name, Model model) {
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());			
+		} else {
+			model.addAttribute("name", "Phoenix");
+		}
+		
+		return "hello2";
+	}
+	
+	@RequestMapping("/calc")
+	public String simpleCalc(@RequestParam(value="calc") String num1, String num2) {
+		
+		
+		return "calc";
+		
+	}
+}
